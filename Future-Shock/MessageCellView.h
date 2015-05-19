d48828ba08249f20d963262845f4c84e4f9b0777
peterWoodrow <peterrallojay@gmail.com>
2015-05-18 23:12:38 -0600

    Added alpha messagecellview design
    will revisit after intermediate tableview lesson

---
+++ Future-Shock/MessageCellView.h
@@ -0,0 +1,14 @@
+//
+//  MessageCellView.h
+//  Future-Shock
+//
+//  Created by Peter Woodrow on 5/18/15.
+//  Copyright (c) 2015 Peter Woodrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface MessageCellView : UIView
+
+@end
---
+++ Future-Shock/MessageCellView.m
@@ -0,0 +1,21 @@
+//
+//  MessageCellView.m
+//  Future-Shock
+//
+//  Created by Peter Woodrow on 5/18/15.
+//  Copyright (c) 2015 Peter Woodrow. All rights reserved.
+//
+
+#import "MessageCellView.h"
+
+@implementation MessageCellView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
