fa47d029769547dd4f3a6bccac1d6aa2f6d727c1
Daniel Dayley <danieldayley@aim.com>
2015-05-29 14:33:50 -0600

    Restrctures how rounds are populated. incomplete

---
+++ RoundHistoryController.h
@@ -0,0 +1,13 @@
+//
+//  RoundHistoryController.h
+//  Future-Shock
+//
+//  Created by Daniel Dayley on 5/29/15.
+//  Copyright (c) 2015 Peter Woodrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RoundHistoryController : NSObject
+
+@end
---
+++ RoundHistoryController.m
@@ -0,0 +1,13 @@
+//
+//  RoundHistoryController.m
+//  Future-Shock
+//
+//  Created by Daniel Dayley on 5/29/15.
+//  Copyright (c) 2015 Peter Woodrow. All rights reserved.
+//
+
+#import "RoundHistoryController.h"
+
+@implementation RoundHistoryController
+
+@end
